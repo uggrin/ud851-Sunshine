31784c0e3389175f4b5db5bce1eb3fb696ee89f5
uggrindev <uggrin@gmail.com>
2017-11-10 21:07:58 +0100

    Add extras to the intent and retrieve the data in the receiving activity

--- S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -1,17 +1,33 @@
 package com.example.android.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
     private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+
+    TextView mDisplayWeatherText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        // TODO (2) Display the weather forecast that was passed from MainActivity
+        mDisplayWeatherText = (TextView) findViewById(R.id.tv_display_weather_text);
+
+        Intent intent = getIntent();
+        String stringData;
+
+        if (intent.getStringExtra("weather") != null) {
+            stringData = getIntent().getStringExtra("weather");
+        } else {
+            stringData = "";
+        }
+
+        mDisplayWeatherText.setText(stringData);
+
     }
 }
